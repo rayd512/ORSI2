a91d7bb1bf977089b57333baeb42d0bf65092ae3
Raynard <rdizon@ualberta.ca>
2022-03-10 13:59:06 -0700

    comments

--- src/app.py
+++ src/app.py
@@ -34,13 +34,13 @@ db = Database()
 def main():
     # Stolen from https://www.geeksforgeeks.org/python-opencv-capture-video-from-camera/
     # define a video capture object
-    # initialize the camera and grab a reference to the raw camera capture
+    # Initialize the camera and grab a reference to the raw camera capture
     camera = PiCamera()
     camera.resolution = (640, 480)
     camera.framerate = 32
     rawCapture = PiRGBArray(camera, size=(640, 480))
 
-    # allow the camera to warmup
+    # Allow the camera to warmup
     time.sleep(0.1)
 
     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
