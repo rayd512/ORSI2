e2a9ef45e6344a91693ee4390da5c515717f248b
rayd512 <rayd512@users.noreply.github.com>
2022-03-30 03:04:41 +0000

    autopep8 action fixes

--- src/models/detect.py
+++ src/models/detect.py
@@ -7,7 +7,8 @@ class Detect:
         self.cascade = cv.CascadeClassifier()
         self.resistors = []
 
-        if not self.cascade.load(cv.samples.findFile("src/models/haarcascade_resistors_0.xml")):
+        if not self.cascade.load(cv.samples.findFile(
+                "src/models/haarcascade_resistors_0.xml")):
             raise Exception("Could not load face cascade!")
 
     def detect(self, frame: List[int]) -> None:
@@ -17,5 +18,5 @@ class Detect:
 
     def draw_ROI(self, frame: List[int]) -> List[int]:
         for (x, y, w, h) in self.resistors:
-            frame = cv.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            frame = cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
         return frame
