89b70abfbb47f369a45020840c939740a2520e0a
Raynard <rdizon@ualberta.ca>
2022-04-02 01:13:22 -0600

    fix logic

--- src/models/detect.py
+++ src/models/detect.py
@@ -143,7 +143,7 @@ class Detect:
                             cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 2, cv2.LINE_AA)
                 continue
 
-            if not fallback_value:
+            if fallback_value:
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
                 cv2.putText(frame, str(resistor_val) + " OHMS", (x + w + 10, y),
                             cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 2, cv2.LINE_AA)
