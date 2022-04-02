9b691a53b3353a5ba55ca477d8908c31f240b548
Raynard <rdizon@ualberta.ca>
2022-04-01 21:11:53 -0600

    refactoring

--- src/models/detect.py
+++ src/models/detect.py
@@ -100,7 +100,7 @@ class Detect:
 
     def show_values(self, frame):
         for i in range(len(self.resistors)):
-            bands = self._find_find_bands(self.resistor_imgs[i])
+            bands = self._find_bands(self.resistor_imgs[i])
             # print(bands)
             # printResult(bands, cliveimg, resClose[i][1])
             x, y, w, h = self.resistors[i]
