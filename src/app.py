4d6e7daf4e09ef2a1f4c804bc1ffad02915525e1
Dan F <daniel_florendo1@yahoo.com>
2022-04-03 15:14:17 -0600

    removed wattage to test start btn

--- src/app.py
+++ src/app.py
@@ -54,7 +54,7 @@ def main():
             # detect.detect(frame)
             start = time.time()
 
-        detect.show_values(frame)
+        # detect.show_values(frame)
 
         # Draw start / end button
         if not has_session:
